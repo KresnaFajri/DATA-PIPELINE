1807727edf35fd3ce24a026e36d3ee4418abe279
Kresna Fajri <kresna.fajri@gmail.com>
2026-03-27 15:48:32 +0700

    updating and adding certain file in dashboard

--- DataAds_Pipeline/dashboard/config.py
+++ DataAds_Pipeline/dashboard/config.py
@@ -7,7 +7,18 @@ load_dotenv(BASE_DIR/".env")
 
 class DashboardConfig:
     
-    DB_AGE = os.getenv("DB_AGE")
-    DB_LOC = os.getenv("DB_LOC")
-    DB_CAM = os.getenv("DB_CAM")
-    DB_KEY = os.getenv("DB_KEY")
+    DB_AGE_PATH = os.getenv("DB_AGE")
+    DB_LOC_PATH = os.getenv("DB_LOC")
+    DB_CAM_PATH = os.getenv("DB_CAM")
+    DB_KEY_PATH = os.getenv("DB_KEY")
+
+    db_loc = os.getenv("db_loc")
+    db_campaign = os.getenv("db_camp")
+    db_keyword = os.getenv("db_keyword")
+    db_age = os.getenv("db_age")
+
+    LOC_TABLE = os.getenv("LOC_TABLE")
+    KEY_TABLE = os.getenv("KEY_TABLE")
+    CAMP_TABLE = os.getenv("CAMP_TABLE")
+    AGE_TABLE = os.getenv("AGE_TABLE")
+
